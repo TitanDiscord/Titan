195654293deb4a1d2a52a0ea99dafc91651f41a6
ANutley <alfienutley1@gmail.com>
2021-09-05 16:56:58 +0100

    Add invite command, and add an invite to the message Titan sends when someone mentions then

--- src/main/java/me/anutley/titan/listeners/MentionListener.java
+++ src/main/java/me/anutley/titan/listeners/MentionListener.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MentionListener extends ListenerAdapter {
 
-    public void onGuildMessageReceived (GuildMessageReceivedEvent event) {
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         if (event.getMessage().getMentionedMembers().size() == 1) {
             if (event.getMessage().getContentRaw().split(" ").length > 1) return;
             if (event.getMessage().getMentionedMembers().get(0).getUser().getId()
@@ -18,7 +18,10 @@ public class MentionListener extends ListenerAdapter {
                                 .setColor(EmbedColour.NEUTRAL.getColour())
                                 .setDescription("All Titan's Command are Slash Commands" +
                                         "\n[GitHub Page](https://github.com/ANutley/Titan/)" +
-                                        "\n[Documentation](https://titan.anutley.me/)")
+                                        "\n[Documentation](https://titan.anutley.me/)" +
+                                        "\n[Invite Me](https://discord.com/api/oauth2/authorize?client_id=853225073023909918&permissions=8&scope=bot%20applications.commands)" +
+                                        "\n[Support Server](https://discord.gg/4ueXW4fwrR)")
+
                                 .setFooter("If you are trying to trigger a tag, the format is @Titan <tag trigger>")
                                 .build()).queue();
             }
