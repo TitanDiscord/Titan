61025a76198f4af00e05acd8afd37c01a4fb1516
ANutley <alfienutley1@gmail.com>
2021-10-18 20:06:57 +0100

    Handle the error received from not being able to send a message to a user

--- src/main/java/me/anutley/titan/listeners/LockdownListener.java
+++ src/main/java/me/anutley/titan/listeners/LockdownListener.java
@@ -4,7 +4,9 @@ package me.anutley.titan.listeners;
 import me.anutley.titan.database.objects.GuildSettings;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.exceptions.ErrorHandler;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.ErrorResponse;
 
 public class LockdownListener extends ListenerAdapter {
 
@@ -14,7 +16,7 @@ public class LockdownListener extends ListenerAdapter {
         if (!event.getGuild().getSelfMember().hasPermission(Permission.KICK_MEMBERS)) return;
             if (new GuildSettings(event.getGuild().getId()).isLockdown()) {
                 event.getUser().openPrivateChannel().complete()
-                        .sendMessage("Lockdown is currently enabled in " + event.getGuild().getName() + ". Please try joining again later!").queue();
+                        .sendMessage("Lockdown is currently enabled in " + event.getGuild().getName() + ". Please try joining again later!").queue(null, new ErrorHandler().ignore(ErrorResponse.CANNOT_SEND_TO_USER));
                 event.getMember().kick("Tried to join while lockdown was enabled").queue();
 
             }
