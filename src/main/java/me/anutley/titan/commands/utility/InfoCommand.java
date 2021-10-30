ee016bfdacdf353d6117bc3232301a54b0b609d6
ANutley <alfienutley1@gmail.com>
2021-10-30 15:18:34 +0100

    Add channel and role to info command

--- src/main/java/me/anutley/titan/commands/utility/InfoCommand.java
+++ src/main/java/me/anutley/titan/commands/utility/InfoCommand.java
@@ -7,18 +7,24 @@ import me.anutley.titan.database.util.WarnUtil;
 import me.anutley.titan.util.RoleUtil;
 import me.anutley.titan.util.enums.EmbedColour;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.utils.TimeFormat;
+import org.apache.commons.lang.StringUtils;
 
 public class InfoCommand {
 
     public static CommandData InfoCommandData = new CommandData("info", "Gives information about various different things")
             .addSubcommands(new SubcommandData("guild", "Gives information about the current guild"))
+            .addSubcommands(new SubcommandData("channel", "Gives information about a channel")
+                    .addOptions(new OptionData(OptionType.CHANNEL, "channel", "The channel to get the info from", true)
+                            .setChannelTypes(ChannelType.TEXT, ChannelType.STAGE, ChannelType.VOICE, ChannelType.CATEGORY)))
+            .addSubcommands(new SubcommandData("role", "Gives information about a role")
+                    .addOption(OptionType.ROLE, "role", "The role to get the info from", true))
             .addSubcommands(new SubcommandData("user", "Gets information about a user")
                     .addOption(OptionType.USER, "user", "The user to get the info from. Leave blank to get information about yourself"))
             .addSubcommands(new SubcommandData("bot", "Gets information about Titan"));
@@ -31,34 +37,72 @@ public class InfoCommand {
 
     }
 
-    private static EmbedBuilder guildInfo(SlashCommandEvent event) {
-        Guild guild = event.getGuild();
-        EmbedBuilder builder = new EmbedBuilder()
-                .setAuthor("Owner: " + guild.getOwner().getUser().getName(), null, guild.getOwner().getUser().getAvatarUrl())
-                .setTitle(guild.getName())
-                .setThumbnail(guild.getIconUrl())
-                .setColor(EmbedColour.NEUTRAL.getColour())
-                .addField("Members", String.valueOf(guild.getMembers().size()), true)
-                .addField("Roles", String.valueOf(guild.getRoles().size()), true)
-                .addField("Emotes", String.valueOf(guild.getEmotes().size()), true)
-                .addField("Verification Level", guild.getVerificationLevel().toString(), true)
-                .addField("Created", TimeFormat.RELATIVE.format(guild.getTimeCreated()), true)
-                .addField("Categories", String.valueOf(guild.getCategories().size()), true)
-                .addField("Channels",
-                        "Text Channels: " + guild.getTextChannels().size() +
-                                "\nVoice Channels: " + guild.getVoiceChannels().size() +
-                                "\nStage Channels: " + guild.getStageChannels().size()
-                        , true)
-                .addField("Boosters", String.valueOf(guild.getBoosters().size()), true)
-                .addField("Boost Count", String.valueOf(guild.getBoostCount()), true)
-                .addField("Boost Level", String.valueOf(guild.getBoostTier()), true)
-                .addField("Total Tags", String.valueOf(TagUtil.getGuildsEmbedTags(guild.getId()).size() + TagUtil.getGuildsTextTags(guild.getId()).size()), true)
-                .addField("Total Warnings", String.valueOf(WarnUtil.getGuildsWarnings(guild.getId()).size()), true);
+    @Command(name = "info.channel", description = "Gives information about a channel", permission = "command.utility.info.channel")
+    public static void channelInfoCommand(SlashCommandEvent event) {
 
-        if (guild.getBoostRole() != null)
-            builder.addField("Boost Role", guild.getBoostRole().getAsMention(), true);
+        event.deferReply().queue();
 
-        return builder;
+        event.getGuild().loadMembers().onSuccess(members -> {
+            GuildChannel channel = event.getOption("channel").getAsGuildChannel();
+
+            EmbedBuilder builder = new EmbedBuilder()
+                    .setTitle("`" + channel.getName() + "` info")
+                    .setColor(EmbedColour.NEUTRAL.getColour())
+                    .addField("Channel", channel.getAsMention(), true)
+                    .addField("ID", channel.getId(), true)
+                    .addField("Type", StringUtils.capitalize(channel.getType().name()), true)
+                    .addField("Time Created", TimeFormat.DATE_TIME_LONG.format(channel.getTimeCreated().toEpochSecond() * 1000), true);
+
+            if (!channel.getType().equals(ChannelType.CATEGORY))
+                builder.addField("Category", channel.getParent().getAsMention(), true);
+
+            if (channel.getType().equals(ChannelType.TEXT)) {
+                TextChannel textChannel = channel.getGuild().getTextChannelById(channel.getId());
+
+                builder.addField("Members", String.valueOf(channel.getMembers().size()), true);
+
+                if (textChannel.getTopic() != null)
+                    builder.addField("Topic", textChannel.getTopic() == null ? "None" : textChannel.getTopic(), true);
+
+                builder.addField("Is News", StringUtils.capitalize(String.valueOf(textChannel.isNews())), true)
+                        .addField("Is NSFW", StringUtils.capitalize(String.valueOf(textChannel.isNSFW())), true);
+
+            }
+
+            if (channel.getType().equals(ChannelType.VOICE) || channel.getType().equals(ChannelType.STAGE)) {
+                VoiceChannel voiceChannel = channel.getGuild().getVoiceChannelById(channel.getId());
+
+                builder.addField("Bitrate", voiceChannel.getBitrate() / 1000 + "kbps", true)
+                        .addField("Region", voiceChannel.getRegion().getName(), true);
+
+                if (voiceChannel.getUserLimit() != 0)
+                    builder.addField("User Limit", voiceChannel.getUserLimit() == 0 ? "None" : String.valueOf(voiceChannel.getUserLimit()), true);
+
+            }
+
+            event.getHook().editOriginalEmbeds(builder.build()).queue();
+        });
+    }
+
+    @Command(name = "info.role", description = "Gives information about a role", permission = "command.utility.info.role")
+    public static void roleInfoCommand(SlashCommandEvent event) {
+        event.deferReply().queue();
+
+        Role role = event.getOption("role").getAsRole();
+
+        event.getHook().editOriginalEmbeds(
+                new EmbedBuilder()
+                        .setTitle("`" + role.getName() + "` info")
+                        .setColor(role.getColor())
+                        .addField("Role", role.getAsMention(), true)
+                        .addField("ID", role.getId(), true)
+                        .addField("Time Created", TimeFormat.DATE_TIME_LONG.format(role.getTimeCreated().toEpochSecond() * 1000), true)
+                        .addField("Position (0 Lowest)", String.valueOf(role.getPosition()), true)
+                        .addField("Hoisted", StringUtils.capitalize(String.valueOf(role.isHoisted())), true)
+                        .addField("Mentionable", StringUtils.capitalize(String.valueOf(role.isMentionable())), true)
+                        .build()
+        ).queue();
+
     }
 
     @Command(name = "info.user", description = "Gets information about a user", permission = "command.utility.info.user")
@@ -107,6 +151,38 @@ public class InfoCommand {
 
                         .build()).queue();
 
+    }
+
+
+    private static EmbedBuilder guildInfo(SlashCommandEvent event) {
+        Guild guild = event.getGuild();
+        EmbedBuilder builder = new EmbedBuilder()
+                .setAuthor("Owner: " + guild.getOwner().getUser().getName(), null, guild.getOwner().getUser().getAvatarUrl())
+                .setTitle(guild.getName())
+                .setThumbnail(guild.getIconUrl())
+                .setColor(EmbedColour.NEUTRAL.getColour())
+                .addField("Time Created", TimeFormat.DATE_TIME_LONG.format(guild.getTimeCreated().toEpochSecond() * 1000), true)
+                .addField("Members", String.valueOf(guild.getMembers().size()), true)
+                .addField("Roles", String.valueOf(guild.getRoles().size()), true)
+                .addField("Emotes", String.valueOf(guild.getEmotes().size()), true)
+                .addField("Verification Level", guild.getVerificationLevel().toString(), true)
+                .addField("Created", TimeFormat.RELATIVE.format(guild.getTimeCreated()), true)
+                .addField("Categories", String.valueOf(guild.getCategories().size()), true)
+                .addField("Channels",
+                        "Text Channels: " + guild.getTextChannels().size() +
+                                "\nVoice Channels: " + guild.getVoiceChannels().size() +
+                                "\nStage Channels: " + guild.getStageChannels().size()
+                        , true)
+                .addField("Boosters", String.valueOf(guild.getBoosters().size()), true)
+                .addField("Boost Count", String.valueOf(guild.getBoostCount()), true)
+                .addField("Boost Level", String.valueOf(guild.getBoostTier()), true)
+                .addField("Total Tags", String.valueOf(TagUtil.getGuildsEmbedTags(guild.getId()).size() + TagUtil.getGuildsTextTags(guild.getId()).size()), true)
+                .addField("Total Warnings", String.valueOf(WarnUtil.getGuildsWarnings(guild.getId()).size()), true);
+
+        if (guild.getBoostRole() != null)
+            builder.addField("Boost Role", guild.getBoostRole().getAsMention(), true);
+
+        return builder;
     }
 
     private static long amountOfMembersInTotalGuild(SlashCommandEvent event) {
